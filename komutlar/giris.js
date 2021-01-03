ee80ae77020edcbd38e58d25799b29ebb4159803
erkan123x <76553944+erkan123x@users.noreply.github.com>
2021-01-03 19:51:37 +0300

    Update giris.js

--- komutlar/giris.js
+++ komutlar/giris.js
@@ -1,36 +1,64 @@
-lient.on("guildMemberAdd", member => {
-  member.send(
-    `:tada: **SelamınAleyküm LegendArrow YOUTUBE #BAKIMDA SUNUCUSUNA HOŞGELDİN BU SUNUCUDA KURALLARI OKUMAK ZORUNLUDUR KURALLARI OKUDUYSAN İYİ EĞLENCELER.**`///bu kısım sunuucuya giren kişiye özelden mesaj atar\n yaparak satır ekleyebilirsiniz ` şu işaretin sonda olmasına dikkat etme şartıyla
-  );
-});
+const Discord = require("discord.js")
+const db = require("quick.db");
+const Canvas = require('canvas')
 
-client.on("guildMemberAdd", async member => {
-  try {
-    let embed = new Discord.RichEmbed();
-    const kullanıcıadı = member.user.username.replace(/\W/g, "");
+    , Image = Canvas.Image
 
-    await client.channels
-      .get("794964054032515073")////////hoş geldin mesajının atılacağı kanal
-      .send(
-        `:tada: SelamınAleyküm HoşGeldin **${member}** 
+    , path = require('path');
+const { registerFont, createCanvas } = require('canvas')
+registerFont('ay.otf', { family: 'SONGER' })
 
-:tada: Seninle beraber **${
-member.guild.memberCount
-        }** Kişiyiz !!
-\n:tada: Kaydının Yapılması İçin Sesli Odaya Gelip Ses Vermen Gerekli. <@&666425273516032045> \n /////// idyi değiştirmeniz gerekiyor
-:tada: **Hesap:** ${
-          new Date().getTime() - member.user.createdAt.getTime() <
-          45 * 24 * 60 * 60 * 1000
-            ? "**Tehlikeli** \:x:"
-            : "**Güvenli** \:white_check_mark:"
-        }
-\n:tada: <@&794565248497483787> Rolündeki Yetkililer Seninle İlgilenecektir.`,////kayıt sorumlusu idsini giriceksiniz 
-        new Discord.Attachment(
-          "https://cdn.discordapp.com/attachments/621045237137276929/623239032536825919/giris.png
-          "/////bunu silebilir ya da başka bir gif ile değiştirbilirsiniz
-        )
+const snekfetch = require('snekfetch');
+const request = require('node-superfetch');
+
+module.exports = async member => {
+    var randomMsg = [];
+    var randomMsg_integer = randomMsg[Math.floor((Math.random() * randomMsg.length))]
+
+  let msj = await db.fetch(`girisM_${member.guild.id}`)
+let user = member.client.users.get(member.id);
+  let memberChannel = await db.fetch(`gcc_${member.guild.id}`)
+  
+  const canvas = Canvas.createCanvas(1280, 720);
+  const ctx = canvas.getContext('2d');
+  
+  const background = await Canvas.loadImage('https://i.hizliresim.com/7Br6Av.jpg');
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+  
+ 
+   ctx.fillStyle = `#ffffff`;
+  ctx.font = `80px "SONGER"`;
+  ctx.textAlign = "center";
+  ctx.fillText(`${member.user.username.toUpperCase()}`, 640, 350);
+ 
+ let avatarURL = member.user.avatarURL || member.user.defaultAvatarURL
+  const { body } = await request.get(avatarURL);
+  const avatar = await Canvas.loadImage(body);
+  
+ctx.beginPath();
+	ctx.lineWidth = 0;
+  ctx.fill()
+	ctx.lineWidth = 0;
+	ctx.arc(580 + 55, 55 + 55, 55, 0, 2 * Math.PI, false);
+	ctx.clip();
+	ctx.drawImage(avatar, 580, 55, 110, 110);
+  
+  
+  const attachment = new Discord.Attachment(canvas.toBuffer(), 'Hvortex.png');
+  if (!member.guild.channels.get(memberChannel)) return
+
+
+ const embed = new Discord.RichEmbed()
+      .setColor("RANDOM")
+
+      .setDescription(
+        `<a:mavi:690267134261329950> ${member.user.username} Adlı Gold üye Katıldı. <a:mavi:690267134261329950>`
       );
-  } catch (err) {
-    console.log(err);
-  }
-});,
+     if (db.has(`üyelikk_${user.id}`)) {
+    if (!member.guild.channels.get(memberChannel)) return;
+  member.guild.channels.get(memberChannel).send(attachment) 
+         member.guild.channels.get(memberChannel).send(embed)
+     } else return
+
+
+}
